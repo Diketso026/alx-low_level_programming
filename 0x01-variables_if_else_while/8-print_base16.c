dbc3b96743a8f8bc480b05aaed4ce9825bc22114
Diketso026 <100201676+Diketso026@users.noreply.github.com>
2022-12-09 23:48:19 +0200

    Create 8-print_base16.c
    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* header comes hers */
+
+/**
+ * main - Entry point
+ *
+ * prints all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ascii = 48;
+
+	int i;
+
+	for (i = 0; i < 16 ; i++)
+	{
+		if (i == 10)
+		{
+			ascii = 87;
+		}
+		putchar(ascii + i);
+	}
+	putchar('\n');
+
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation

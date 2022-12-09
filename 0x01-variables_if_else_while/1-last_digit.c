982585082e07373482f975747516b233ddb1f78d
Diketso026 <100201676+Diketso026@users.noreply.github.com>
2022-12-09 23:27:54 +0200

    Update 1-last_digit.c
    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,11 +4,14 @@
 
 /* more headers goes there */
 
+
 /**
- * main execute files 
+ * main - Entry point
+ *
  * if the last digit of n is greater than 5: the string and is greater than 5
  * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0: the string and is less than 6 and not 0 
+ * if the last digit of n is less than 6 and not 0:
+ * the string and is less than 6 and not 0
  * Return: Always 0 (Success)
  */
 

754a29e7ac5dc6886328b58162b61f08a0ba380e
Diketso026 <100201676+Diketso026@users.noreply.github.com>
2022-12-09 23:52:15 +0200

    Update 1-last_digit.c
    update

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,16 +2,13 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
-
+/* header comes here */
 
 /**
  * main - Entry point
  *
- * if the last digit of n is greater than 5: the string and is greater than 5
- * if the last digit of n is 0: the string and is 0
- * if the last digit of n is less than 6 and not 0:
- * the string and is less than 6 and not 0
+ * ssign a random number to the variable n each time it is executed
+ *
  * Return: Always 0 (Success)
  */
 

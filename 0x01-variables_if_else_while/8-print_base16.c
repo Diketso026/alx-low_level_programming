423041a3e9dcfe61d8d07c91109502dfc651795d
Diketso026 <100201676+Diketso026@users.noreply.github.com>
2022-12-10 05:35:37 +0200

    Update 8-print_base16.c
    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
+
+/* header comes here */
 
 /**
  * main - Entry point
  *
- * prints all the numbers of base 16 in lowercase
+ * prints all the numbers of base 16 in lowercase,
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int ascii = 48;

41578ed611b0b696800a430ed9da14455342767c
Diketso026 <100201676+Diketso026@users.noreply.github.com>
2022-12-10 05:48:20 +0200

    Update 8-print_base16.c
    update

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-#include <unistd.h>
-
-/* header comes here */
 
 /**
  * main - Entry point
- *
- * prints all the numbers of base 16 in lowercase,
- *
  * Return: Always 0 (Success)
  */
 

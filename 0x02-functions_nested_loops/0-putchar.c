39d7304549df6c9dcb619c583766c3df20d87c8a
Diketso026 <100201676+Diketso026@users.noreply.github.com>
2022-12-15 02:32:47 +0200

    Update 0-putchar.c
    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Alwys 0 (Success)
+ */
+int main(void)
+{
+	char abe[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(abe[c]);
+	}
+	_putchar('\n');
+	return (0);
+}

601d361adc1591916d085f765d75543c78b5f563
Diketso026 <awork026@gmail.com>
2023-01-05 19:49:52 -0800

    completed project

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  _sqrt_recursion - main function
+ *  @n: int n
+ *  Return: int (returning function against itself
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+
+/**
+ * _sqrt -square root recursion
+ * integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n 0< 0)
+		return (-1);
+	if ((i * i) > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
